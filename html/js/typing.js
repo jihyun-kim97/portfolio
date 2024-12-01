e53baee558ea631d4a2ab7b5f474c8ab4639a14f
elbertinga <eodml777@naver.com>
2024-12-01 09:55:30 +0900

    ji upload of files

---
+++ html/js/typing.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    function typeText(element, text, speed) {
+        let index = 0;
+        function type() {
+            if (index < text.length) {
+                $(element).append(text[index++]);
+                setTimeout(type, speed);
+            }
+        }
+        type();
+    }
+
+    let speed = 50; // 타이핑 속도 (밀리초 단위)
+    $('.type-item').each(function (i, element) {
+        let text = $(element).text();
+        $(element).text('');
+        setTimeout(function () {
+            typeText(element, text, speed);
+        }, i * 1000); // 항목마다 타이핑 시작 시간을 달리 함
+    });
+    let aboutTop = $('#about_me').offset().top;
+    let ht =$(window).height();
+    $(window).scroll(function () {
+        let sc = $(window).scrollTop();
+        
+        if (sc >= ht) {
+            $('.modal').hide();
+        }else{
+            $('.modal').show();
+        }
+    })
+});
