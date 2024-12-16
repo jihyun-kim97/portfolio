574e62a3109f54f01c8a56bdda3dfa60906b3e97
jihyun-kim97 <skdn788@gmail.com>
2024-12-16 22:45:19 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('.canvas > ul> li').click(function () {
-        let color = $(this).css('background-color');
-        console.log(color)
-        $('.mainSvg path').css({ 'fill': color });
-    });
-});
